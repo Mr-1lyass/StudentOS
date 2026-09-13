60d6f4732a48c86adaa408f35dface8be3dcd07a
1lyass <ilyassbinche@gmail.com>
2026-09-13 18:47:52 +0200

    v2 (git)

--- scripts/config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://xmynevhdbrrdmwwyvhfo.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_drcO7qPquTmUYI67S7UTtw_NGuhMiDQ';
-
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
